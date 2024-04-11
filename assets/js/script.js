042817d50315f27a5fa150ff074857573836678d
alarrabee <alexlarrabee10@gmail.com>
2024-04-10 21:40:29 -0500

    link search bar to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,51 @@
 
 
+const searchBar = document.getElementById('searchBar');
+const searchBtn = document.getElementById('searchBtn');
+
+
 
 
 const xenoCantoApi = 'https://xeno-canto.org/api/2/recordings?query=bearded+bellbird+q:A';
 
+function submitBird() {
+  let userSearchArray = JSON.parse(localStorage.getItem('theBird')) || [];
 
-      fetch(xenoCantoApi)
-      .then(function(response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-      });
+  const searchRequest = {
+  searchBar: searchBar.value
+  }
+
+  userSearchArray.push(searchRequest);
+  localStorage.setItem('theBird', JSON.stringify(userSearchArray));
+
+}
+
+
+searchBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  submitBird();
+});
+
+
+
+fetch(xenoCantoApi)
+
+.then(function(response) {
+  console.log(response);
+  return response.json();
+})
+
+  .then(function(data) {
+    console.log(data);
+    
+});
+
+
+
+
+
+
+
 
       fetch('https://nuthatch.lastelm.software/v2/birds?page=1&pageSize=25&name=bluebird&operator=AND', { //Get some wrens
         headers: {
