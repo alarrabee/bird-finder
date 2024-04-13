86df8dfff48a2fa5bfeb2b33dbc740016189fa2d
alarrabee <alexlarrabee10@gmail.com>
2024-04-13 09:36:07 -0500

    diplay facts to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,38 @@ function getFacts(bird) {
     console.log(birdData); 
   }) 
 };
+
+//Displays the specified bird facts retrieved from the bird object from getFacts(bird)
+function displayFacts(birdData) {
+  const name = birdData.entities[0].name;
+
+
+  const nameEl = document.getElementById('name');
+  const sciNameEl = document.getElementById('sciName');
+  const familyEl = document.getElementById('family');
+  const orderEl = document.getElementById('order');
+  const regionEl = document.getElementById('region');
+  const statusEl = document.getElementById('status');
+  const sizeEl = document.getElementById('size');
+  const wingSpanEl = document.getElementById('wingSpan');
+
+
+  nameEl.textContent = `Common Name: ${birdData.entities[0].name}`;
+  sciNameEl.textContent = `Scientific Name: ${birdData.entities[0].sciName}`;
+  familyEl.textContent = `Family: ${birdData.entities[0].family}`;
+  orderEl.textContent = `Order: ${birdData.entities[0].order}`;
+  regionEl.textContent = `Region: ${birdData.entities[0].region}`;
+  statusEl.textContent = `Conservation Status: ${birdData.entities[0].status}`;
+  sizeEl.textContent = `Size: Measures ${birdData.entities[0].lengthMin}-${birdData.entities[0].lengthMax} cm long`;
+  wingSpanEl.textContent = `Wingspan: Measures ${birdData.entities[0].wingspanMin}-${birdData.entities[0].wingspanMax} cm across`;
+
+
+  birdFacts.append(nameEl);
+  birdFacts.append(sciNameEl);
+  birdFacts.append(familyEl);
+  birdFacts.append(orderEl);
+  birdFacts.append(regionEl);
+  birdFacts.append(statusEl);
+  birdFacts.append(sizeEl);
+  birdFacts.append(wingSpanEl);
+}
